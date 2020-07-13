f5b8d49b43be2a75285feb9dbd6cf5be44e8a6f9
Richard Pawson <rpawson@nakedobjects.net>
2020-07-13 14:16:53 +0100

    Transfer in from local repository

---
+++ Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace OOPDraw
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(Pen p, int x1, int y1, int x2, int y2) : base(p, x1, y1, x2, y2)
+        {
+        }
+
+        public Rectangle(Pen p, int x1, int y1) : base(p, x1, y1)
+        {
+        }
+
+        public override void Draw(Graphics g)
+        {
+            (int x, int y, int w, int h) = EnclosingRectangle();
+            g.DrawRectangle(Pen, x, y, w, h);
+        }
+
+        public bool FullySurrounds(Shape s)
+        {
+            (int x, int y, int w, int h) = this.EnclosingRectangle();
+            (int xs, int ys, int ws, int hs) = s.EnclosingRectangle();
+            return x < xs && y < ys && x + w > xs + ws && y + h > ys + hs;
+        }
+
+        public override Shape Clone()
+        {
+            return new Rectangle(Pen, X1, Y1, X2, Y2);
+        }
+    }
+}
